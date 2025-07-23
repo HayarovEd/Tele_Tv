ca4d8bf83998560ce90adfd3d99b738ee4f117a7
Impuls <edurd@rambler.ru>
2025-07-23 09:51:18 +0500

    correct splash

--- splash_screen/src/main/java/com/edurda77/impuls/tele_tv/splash/SplashScreen.kt
+++ splash_screen/src/main/java/com/edurda77/impuls/tele_tv/splash/SplashScreen.kt
@@ -1,19 +1,14 @@
 package com.edurda77.impuls.tele_tv.splash
 
-import androidx.compose.foundation.Image
+import android.widget.ImageView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.edurda77.impuls.tele_tv.resources.R
 import com.edurda77.impuls.tele_tv.resources.theme.Tele_TvTheme
 import kotlinx.coroutines.flow.collectLatest
@@ -42,21 +37,31 @@ private fun SplashScreenScreen(
     modifier: Modifier = Modifier,
 ) {
     Box(modifier = modifier.fillMaxSize()) {
-        Image(
+        AndroidView(
             modifier = modifier
                 .fillMaxSize(),
-            painter = painterResource(id = R.drawable.scale_1200),
+            factory = { context ->
+                ImageView(context).apply {
+                    setImageResource(R.drawable.splash_background)
+                    scaleType = ImageView.ScaleType.FIT_XY
+                }
+            },
+        )
+        /*Image(
+            modifier = modifier
+                .fillMaxSize(),
+            painter = painterResource(id = R.drawable.splash_background),
             contentScale = ContentScale.FillBounds,
             contentDescription = "",
-        )
-        Image(
+        )*/
+        /*Image(
             modifier = modifier
                 .align(Alignment.Center)
                 .fillMaxWidth(0.85f),
             painter = painterResource(id = R.drawable.logo62),
             contentScale = ContentScale.FillWidth,
             contentDescription = "",
-        )
+        )*/
     }
 }
 
