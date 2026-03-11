3d20c66a4bbbf534e5af07b63b6164d1f8c41bc4
Impuls <edurd@rambler.ru>
2026-03-11 14:10:17 +0500

    update libs

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -29,10 +29,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
+
+kotlin {
+    jvmToolchain(11)
+}
+
 
 dependencies {
     implementation(project(":domain"))
